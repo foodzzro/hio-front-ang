181e391364874fec871dd93b49b297bba029ac14
Silviu Popa <silviucpopa@gmail.com>
2019-01-22 17:10:20 +0200

    add docker configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+
 export const environment = {
-  production: true
+  production: true,
+  gMaps_API_KEY: "AIzaSyDOtP3SUev1_kSwIEuZ8N_cT3mKqg7KZxg",
+  gMaps_URL: "https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=",
+  service_URL: "/api",
+
+  userRoles: [
+    "ROLE_CLIENT",
+    "ROLE_ADMIN",
+    "ROLE_MANAGER"
+  ]
 };
